3433c39c26dc2194836b0fbb6e0e41c534b8be03
anuroopsk79@gmail.com <anuroopsk79@gmail.com>
2020-09-05 08:14:14 -0700

    2nd commit to B1

--- src/main/java/com/anuroop/git/App3.java
+++ src/main/java/com/anuroop/git/App3.java
@@ -1,6 +1,8 @@
 package com.anuroop.git;
 
 public class App3 {
+	
+	private static String thisIsB1Change;
 
 	public App3() {
 
@@ -10,8 +12,7 @@ public class App3 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-
+		System.out.println(thisIsB1Change);
 	}
 
 }
