5a1b7e320d571ed01c0d4f5dc817ef99ca9eeaae
anuroopsk79@gmail.com <anuroopsk79@gmail.com>
2020-09-05 07:19:19 -0700

    Initial commit

---
+++ src/main/java/com/anuroop/git/App.java
@@ -0,0 +1,13 @@
+package com.anuroop.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
