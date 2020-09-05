cf3ef5390ad24a1eec5220e916a6ff410f50af8c
anuroopsk79@gmail.com <anuroopsk79@gmail.com>
2020-09-05 08:07:41 -0700

    commit to B1

--- src/main/java/com/anuroop/git/App3.java
+++ src/main/java/com/anuroop/git/App3.java
@@ -3,14 +3,14 @@ package com.anuroop.git;
 public class App3 {
 
 	public App3() {
-		// TODO Auto-generated constructor stub
+
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 
 	}
 
